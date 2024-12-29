07cdfdd1fbc002ae4e3c6479947de54314d8a61d
David Kolosza <david.kolosza@gmx.de>
2024-12-29 15:51:59 +0100

    navbar animation mobile

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -8,11 +8,22 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
+
 export class NavbarComponent {
+  isMenuOpen = false; // Standard: Menü geschlossen
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen; // Umschalten des Zustands
+
+  }
+
   scrollToElement(id: string) {
     const element = document.getElementById(id);
     if (element) {
-      element.scrollIntoView({ behavior: 'smooth', inline: 'start' });
+      element.scrollIntoView({ behavior: 'smooth', inline: 'start'});
     }
+    this.isMenuOpen = false; // Schließen des Menüs nach Navigation
+    this.toggleMenu();
+
   }
 }
